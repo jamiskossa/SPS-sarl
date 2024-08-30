572cea54a3e5e336ccef678a8a4e5f09452a1cf8
jamiskossa <jamiskossa@gmail.com>
2024-08-30 13:37:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// assets/js/script.js
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 5000); // Change d'image toutes les 5 secondes
+}
